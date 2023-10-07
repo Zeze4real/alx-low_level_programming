80cd5d33b510d420b385ddbb291fdd47ec3e26f9
ISMAIL Abdulkarim Shuaib <ismailabdulshuaib@gmail.com>
2023-10-07 01:45:24 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - A program that prints a line with puts funtion
- *Return: 0(success)
+ * main - display  "Programming is like building a multilingual puzzle"
+ * Return: 0 and exit the program
  */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
