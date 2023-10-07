271fd4678d3af3af09466bf2d7b689f89c403d79
ISMAIL Abdulkarim Shuaib <ismailabdulshuaib@gmail.com>
2023-10-07 11:06:35 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,30 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
 /**
  * main - Entry point of the program
  *
- * Description: This program generates a random number 'n', calculates its
- * last digit 'm', and classifies 'm' based on whether it's greater than 5,
- * less than 6 and not 0, or equal to 0.
+ * Description: This program prints the lowercase alphabet from 'a' to 'z'
+ * followed by a newline character.
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
-	int n, m;
+	char a;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	m = n % 10;
-	printf("Last digit of %d ", n);
-	if (m == 0)
-		printf("is %d and is 0\n", m);
-	else if (m > 5)
-		printf("is %d and is greater than 5\n", m);
-	else
-		printf("is %d and is less than 6 and not 0\n", m);
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	putchar('\n');
 	return (0);
 }
