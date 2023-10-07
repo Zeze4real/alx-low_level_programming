b7012a99c1c09e649e31e587a56a2b0620fb2c91
ISMAIL Abdulkarim Shuaib <ismailabdulshuaib@gmail.com>
2023-10-06 23:33:20 -0700

    printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Displays 'with proper grammar' with new line 
+ * main - displays "with proper grammar" with new line 
  *
  * Return: 0 and exit the program
  */
 int main (void)
 {
-	printf('with profer grammar, but the outcome is a piece of art,\n');
+	printf("with profer grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
